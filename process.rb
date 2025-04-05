c6b0e16a3223d236b32243f03ffadaec6bbaa081
Srinivasan Raguraman <srinivasan.raguraman@gmail.com>
2025-04-06 04:10:13 +1000

    use symbols for hash lookup

--- process.rb
+++ process.rb
@@ -6,39 +6,39 @@ require 'erb'
 
 def extract_content_and_config(file)
   content, config_string = File.read(file).split(/^__CONFIG__$/, 2)
-  config = YAML.safe_load(ERB.new(config_string).result, permitted_classes: [String, Symbol, Integer, Hash, Array, TrueClass, FalseClass])
+  config = YAML.safe_load(ERB.new(config_string).result, permitted_classes: [String, Symbol, Integer, Hash, Array, TrueClass, FalseClass], symbolize_names: true)
   [content.strip, config]
 end
 
-def generate_file(content, vars, env, output_path)
+def generate_file(content, vars, output_path)
+  env = vars[:env][:default]
   output = content.gsub(/\$\w+\b/) do |match|
-    key = match[1..]
-    (vars[key][env] || vars[key]['default']).to_s.gsub(/\$env\b/, env)
+    key = match[1..].to_sym
+    (vars[key][env.to_sym] || vars[key][:default]).to_s.gsub(/\$env\b/, env)
   end
-
   FileUtils.mkdir_p(File.dirname(output_path))
-  
   File.write(output_path, output)
+  # puts output
   puts "Generated: #{output_path}"
 end
 
 def process_template(template_file, output_dir = 'build')
   content, config = extract_content_and_config(template_file)
   
-  config['env'].each do |env|
-    vars = config['vars'].merge('env' => env)
+  config[:env].each do |env|
+    vars = config[:vars].merge(:env => { :default => env })
     output_path = File.join(output_dir, env, File.basename(template_file, '.*'))
     
-    generate_file(content, vars, env, output_path)
+    generate_file(content, vars, output_path)
   end
 end
 
 # Main execution
 if $PROGRAM_NAME == __FILE__
-  if ARGV.empty?
+  template_file, output_dir = ARGV
+  unless template_file
     $stderr.puts "Usage: #{$PROGRAM_NAME} <template_file> [output_dir]"
     exit 1
   end
-
-  process_template(ARGV[0], ARGV[1])
+  process_template(template_file, output_dir)
 end
